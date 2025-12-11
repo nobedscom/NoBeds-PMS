525430d15d4028a7a1f8a32e587b825e86542557
JUST TRADE Ltd <support@nobeds.com>
2025-12-11 10:58:39 +0200

    Initial Commit

---
+++ NoBeds PMS/NoBeds_PMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoBeds_PMSApp.swift
+//  NoBeds PMS
+//
+//  Created by Admin on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoBeds_PMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
